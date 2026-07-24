ae21fe6f78fe3ea5cb520d5d33fb7fe4db109234
NatanJrr98 <natancezar@hotmail.com>
2026-07-24 14:14:16 -0300

    Arrumando Menus

--- menu-mobile.js
+++ menu-mobile.js
@@ -1,6 +1,7 @@
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
+let linksMenu = document.querySelectorAll('.menu-mobile nav ul li a');
 
 btnMenu.addEventListener('click', ()=> {
     menu.classList.add('abrir-menu')
@@ -13,3 +14,10 @@ menu.addEventListener('click', ()=> {
 overlay.addEventListener('click', ()=> {
     menu.classList.remove('abrir-menu')
 })
+
+linksMenu.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('abrir-menu');
+        overlay.classList.remove('abrir-menu');
+    });
+});
